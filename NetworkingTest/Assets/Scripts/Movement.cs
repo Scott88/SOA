e034996e87b61d02e2f834151bfa2298b1cf0e0d
Nicholas MacDonald <nick_mac91@hotmail.com>
2015-01-13 15:41:00 -0400

    -Made another attempt at turn based movement

--- NetworkingTest/Assets/Scripts/Movement.cs
+++ NetworkingTest/Assets/Scripts/Movement.cs
@@ -5,12 +5,22 @@ public class Movement : MonoBehaviour {
 
     public float speed = 1f;
 
-    bool myTurn;
+    public bool myTurn = false;
+
+    void Start()
+    {
+        if (Network.isServer)
+        {
+            PlayerSpawner spawner = FindObjectOfType<PlayerSpawner>();
+
+            spawner.AddPlayer(this);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Network.isClient || Network.isServer)
+        /*if (Network.isClient || Network.isServer)
         {
             if (networkView.isMine && myTurn)
             {
@@ -59,8 +69,76 @@ public class Movement : MonoBehaviour {
             {
                 transform.Translate(1.0f, 0.0f, 0.0f);
             }
-        }
+        }*/
 	}
+
+    void OnGUI()
+    {
+        if (Network.isClient || Network.isServer)
+        {
+            if (networkView.isMine && myTurn)
+            {
+                if (GUI.Button(new Rect(Screen.width * (0.075f), Screen.height * (0.825f), Screen.width * (0.05f), Screen.height * (0.05f)),
+                               "^"))
+                {
+                    transform.Translate(0.0f, 1.0f, 0.0f);
+                    myTurn = false;
+                }
+
+                if (GUI.Button(new Rect(Screen.width * (0.025f), Screen.height * (0.875f), Screen.width * (0.05f), Screen.height * (0.05f)),
+                               "<"))
+                {
+                    transform.Translate(-1.0f, 0.0f, 0.0f);
+                    myTurn = false;
+                }
+
+                if (GUI.Button(new Rect(Screen.width * (0.125f), Screen.height * (0.875f), Screen.width * (0.05f), Screen.height * (0.05f)),
+                               ">"))
+                {
+                    transform.Translate(1.0f, 0.0f, 0.0f);
+                    myTurn = false;
+                }
+
+                if (GUI.Button(new Rect(Screen.width * (0.075f), Screen.height * (0.925f), Screen.width * (0.05f), Screen.height * (0.05f)),
+                               "v"))
+                {
+                    transform.Translate(0.0f, -1.0f, 0.0f);
+                    myTurn = false;
+                }
+
+                if (!myTurn)
+                {
+                    networkView.RPC("TurnOver", RPCMode.Others);
+                }
+            }
+        }
+        else
+        {
+            if (GUI.Button(new Rect(Screen.width * (0.075f), Screen.height * (0.825f), Screen.width * (0.05f), Screen.height * (0.05f)),
+                               "^"))
+            {
+                transform.Translate(0.0f, 1.0f, 0.0f);
+            }
+
+            if (GUI.Button(new Rect(Screen.width * (0.025f), Screen.height * (0.875f), Screen.width * (0.05f), Screen.height * (0.05f)),
+                           "<"))
+            {
+                transform.Translate(-1.0f, 0.0f, 0.0f);
+            }
+
+            if (GUI.Button(new Rect(Screen.width * (0.125f), Screen.height * (0.875f), Screen.width * (0.05f), Screen.height * (0.05f)),
+                           ">"))
+            {
+                transform.Translate(1.0f, 0.0f, 0.0f);
+            }
+
+            if (GUI.Button(new Rect(Screen.width * (0.075f), Screen.height * (0.925f), Screen.width * (0.05f), Screen.height * (0.05f)),
+                           "v"))
+            {
+                transform.Translate(0.0f, -1.0f, 0.0f);
+            }
+        }
+    }
 
     [RPC]
     void TurnOver()
