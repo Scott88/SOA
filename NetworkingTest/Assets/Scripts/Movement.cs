bc5d989172694d128c2bbab13be456a039ce992d
Nicholas MacDonald <nick_mac91@hotmail.com>
2015-01-13 14:28:53 -0400

    -Implemented turn based grid movement

--- NetworkingTest/Assets/Scripts/Movement.cs
+++ NetworkingTest/Assets/Scripts/Movement.cs
@@ -4,16 +4,67 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
     public float speed = 1f;
+
+    bool myTurn;
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (networkView.isMine)
+        if (Network.isClient || Network.isServer)
         {
-            float x = Input.GetAxis("Horizontal");
-            float y = Input.GetAxis("Vertical");
+            if (networkView.isMine && myTurn)
+            {
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    transform.Translate(0.0f, 1.0f, 0.0f);
+                    myTurn = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    transform.Translate(0.0f, -1.0f, 0.0f);
+                    myTurn = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.A))
+                {
+                    transform.Translate(-1.0f, 0.0f, 0.0f);
+                    myTurn = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.D))
+                {
+                    transform.Translate(1.0f, 0.0f, 0.0f);
+                    myTurn = false;
+                }
 
-            transform.Translate(x * Time.deltaTime * speed, y * Time.deltaTime * speed, 0f);
+                if (!myTurn)
+                {
+                    networkView.RPC("TurnOver", RPCMode.Others);
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.Translate(0.0f, 1.0f, 0.0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.Translate(0.0f, -1.0f, 0.0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.Translate(-1.0f, 0.0f, 0.0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.Translate(1.0f, 0.0f, 0.0f);
+            }
         }
 	}
+
+    [RPC]
+    void TurnOver()
+    {
+        myTurn = true;
+    }
 }
