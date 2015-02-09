fb20a484614d0065094f927217b875450daa0509
Nicholas MacDonald <nick_mac91@hotmail.com>
2015-02-09 11:49:08 -0400

    -Added a tagged collision trigger

---
+++ SpiritOfAdventure/Assets/Scripts/GameScripts/Triggers/TaggedCollisionTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TaggedCollisionTrigger : Triggerable
+{
+    public string target;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.tag == target)
+        {
+            Trigger();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == target)
+        {
+            Trigger();
+        }       
+    }
+}
