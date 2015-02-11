85ff93077411560f3ae19e7a35f23db36c417028
Nicholas MacDonald <nick_mac91@hotmail.com>
2015-02-11 15:10:04 -0400

    -Forgot to push spread chances

---
+++ SpiritOfAdventure/Assets/CashBasher/Scripts/Gameplay/SpreadChances.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpreadChances : MonoBehaviour {
+
+    public float spreadUp = 0;
+    public float spreadDiagonalUp = 0;
+    public float spreadSides = 0f;
+    public float spreadDiagonalDown = 0f;
+    public float spreadDown = 0f;
+}
