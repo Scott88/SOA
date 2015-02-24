799ba23c28bf7b2133000671342bd46ff2aba8de
Nicholas MacDonald <nick_mac91@hotmail.com>
2015-02-24 09:25:53 -0400

    -Implemented hints into the loading screen -MSIP switch

--- SpiritOfAdventure/Assets/Scripts/GameScripts/GUIScripts/LevelLoader.cs
+++ SpiritOfAdventure/Assets/Scripts/GameScripts/GUIScripts/LevelLoader.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class LevelLoader : MonoBehaviour
 {
+    public TextMesh hintDisplay;
+
 	void Start()
 	{
+        hintDisplay.text = FindObjectOfType<HintList>().GetRandomHint();
+
 		if (LevelQueue.UsesInt())
 		{
 			Application.LoadLevel(LevelQueue.GetNextLevel());
