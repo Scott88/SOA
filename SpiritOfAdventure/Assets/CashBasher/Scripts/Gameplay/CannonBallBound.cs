a5c1caa4a4d8630dbcf620373ec487c02727d869
Nicholas MacDonald <nick_mac91@hotmail.com>
2015-02-25 13:53:52 -0400

    -woops

---
+++ SpiritOfAdventure/Assets/CashBasher/Scripts/Gameplay/CannonBallBound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBallBound : MonoBehaviour
+{
+    CameraMan cameraMan;
+
+    void Start()
+    {
+        cameraMan = FindObjectOfType<CameraMan>();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.tag == "CannonBall")
+        {
+            cameraMan.StopFollowing();
+
+            coll.gameObject.GetComponent<SelfDeleter>().lifeTime = 1f;  
+        }
+    }
+
+}
---
+++ SpiritOfAdventure/Assets/Scripts/GameScripts/StarSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarSpawner : MonoBehaviour
+{
+    public GameObject earnedStar;
+
+    public void Go(int numStars, Camera guiCamera, Camera playerCamera)
+    {
+        StartCoroutine(CollectStars(numStars, guiCamera, playerCamera));
+    }
+
+    IEnumerator CollectStars(int numStars, Camera guiCamera, Camera playerCamera)
+    {
+        float delay = 0.1f;
+        float delayIncrement = (0.75f + (float)numStars * 0.05f) / (float)numStars;
+
+        for (int j = 0; j < numStars; j++)
+        {
+            Vector3 spawnPoint = guiCamera.ScreenToWorldPoint(playerCamera.WorldToScreenPoint(transform.position));
+
+            GameObject star = Instantiate(earnedStar, spawnPoint, Quaternion.identity) as GameObject;
+
+            EarnedStar starGiver = star.GetComponent<EarnedStar>();
+
+            starGiver.delay = delay;
+            starGiver.Go();
+
+            yield return new WaitForSeconds(delayIncrement);
+        }
+
+        Destroy(gameObject);
+    }
+}
